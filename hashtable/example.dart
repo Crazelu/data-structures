8bba81de4fb3817cf3269310979186e62cf02a70
Crazelu <eberelucky64@gmail.com>
2022-09-26 22:16:44 +0100

    feat: adds an implementation of HashTable

---
+++ hashtable/example.dart
@@ -0,0 +1,45 @@
+import 'hashtable.dart';
+
+void main() {
+  final sophiaTheCat = const Cat("Sophia", "Sphynx");
+  final jamesTheCat = const Cat("James", "Sphynx");
+  final hashTable = HashTable<Object, String>();
+
+  hashTable.insert("Egypt", "Cairo");
+  hashTable.insert("Ghana", "Accra");
+  hashTable.insert("Sierra Leone", "Freetown");
+  hashTable.insert("Togo", "Lome");
+  hashTable.insert("Kenya", "Nairobi");
+  hashTable[sophiaTheCat] = "Sophia";
+  hashTable[jamesTheCat] = "James";
+  hashTable["Nigeria"] = "Abuja";
+
+  print(hashTable);
+  print(hashTable.get("Nigeria"));
+  print(hashTable.get("Egypt"));
+  print(hashTable["Kenya"]);
+  print(hashTable.get("Togo"));
+  print(hashTable.get(jamesTheCat));
+
+  hashTable.remove("Ghana");
+  print(hashTable);
+  print(hashTable.get("Ghana"));
+  print(hashTable[sophiaTheCat]);
+}
+
+class Cat {
+  final String name;
+  final String breed;
+
+  const Cat(this.name, this.breed);
+
+  @override
+  int get hashCode {
+    return name.hashCode * breed.hashCode;
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is Cat && other.breed == breed && other.name == name;
+  }
+}
---
+++ hashtable/hashtable.dart
@@ -0,0 +1,98 @@
+class _Node<K extends Object, V extends Object> {
+  _Node<K, V>? next;
+  K key;
+  V value;
+
+  _Node({
+    required this.key,
+    required this.value,
+    this.next,
+  });
+}
+
+class HashTable<K extends Object, V extends Object> {
+  HashTable({int maxSize = 13}) {
+    _maxSize = maxSize;
+    _table = List<_Node<K, V>?>.generate(_maxSize, (index) => null);
+  }
+  late int _maxSize;
+  late List<_Node<K, V>?> _table;
+
+  int _hash(K key) {
+    return key.hashCode % _maxSize;
+  }
+
+  void insert(K key, V value) {
+    remove(key);
+    final hashValue = _hash(key);
+    final newNode = _Node<K, V>(key: key, value: value);
+    _Node<K, V>? lastNode = _table[hashValue];
+
+    if (lastNode == null) {
+      _table[hashValue] = newNode;
+      return;
+    }
+
+    while (lastNode?.next != null) {
+      lastNode = lastNode?.next;
+    }
+    lastNode?.next = newNode;
+  }
+
+  V? get(K key) {
+    _Node<K, V>? node = _table[_hash(key)];
+    while (node != null) {
+      if (node.key == key) return node.value;
+      node = node.next;
+    }
+    return null;
+  }
+
+  void remove(K key) {
+    final hashValue = _hash(key);
+
+    _Node<K, V>? previousNode;
+    _Node<K, V>? node = _table[hashValue];
+
+    while (node != null) {
+      if (node.key == key) {
+        if (previousNode == null) {
+          _table[hashValue] = node.next;
+        } else {
+          previousNode.next = node.next;
+        }
+        return;
+      }
+      previousNode = node;
+      node = node.next;
+    }
+  }
+
+  void clear() {
+    _table = List<_Node<K, V>?>.generate(_maxSize, (index) => null);
+  }
+
+  void operator []=(K key, V value) {
+    insert(key, value);
+  }
+
+  V? operator [](K key) => get(key);
+
+  @override
+  String toString() {
+    String result = "";
+    for (var column in _table) {
+      _Node? node = column;
+      while (node != null) {
+        result += "${node.key}: ${node.value}, ";
+        node = node.next;
+      }
+    }
+
+    if (result.isNotEmpty) {
+      result = result.substring(0, result.length - 2);
+    }
+
+    return "{$result}";
+  }
+}
